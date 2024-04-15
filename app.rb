c7f89abafa18691f54721b95d5516ab8f733c5c2
1110lias <elias-b@outlook.fr>
2024-04-15 18:15:33 +0200

    ras

--- app.rb
+++ app.rb
@@ -1,22 +1,18 @@
-# lignes très pratiques qui appellent les gems du Gemfile. On verra plus tard comment s'en servir ;) - ça évite juste les "require" partout
 require 'bundler'
 Bundler.require
 
-# lignes qui appellent les fichiers lib/user.rb et lib/event.rb
-# comme ça, tu peux faire User.new dans ce fichier d'application. Top.
 require_relative 'lib/user'
 require_relative 'lib/event'
-
-
-# Maintenant c'est open bar pour tester ton application. Tous les fichiers importants sont chargés
-# Tu peux faire User.new, Event.new, binding.pry, User.all, etc.
 
 # users
 julie = User.new("emailjulie", 22)
 julien = User.new("email2", 32)
+julienne = User.new("emailf52", 34)
+julian = User.new("email4", 35)
+julio = User.new("email20", 38)
 
 # events
-dentist = Event.new("dentist","2024-12-01 11:00", 20)
+dentist = Event.new("dentist","2024-04-15 16:45", 20)
 dentist2 = Event.new("dentist2","2023-11-11 14:00", 25)
 coiffeur = Event.new("coiffeur", "2024-12-02", 30)
 pointquo = Event.new("standup quotidien", "2019-01-13 09:00", 10, ["truc@machin.com", "bidule@chose.fr"])
--- lib/user.rb
+++ lib/user.rb
@@ -13,5 +13,24 @@ class User
     def self.all
         return @@all_user
     end
+
+    def show_itself
+        return self
+    end
+
+    def show_name
+        return 
+    end
+
+    def self.find_by_email(email)
+        # email = @user_email
+        # puts email
+        @@all_user.each do |iter|
+            if iter.user_email == email
+                puts "oui #{iter.user_email} voila son age: #{iter.user_age} et voila son id: #{iter.show_itself} et c'est quoi ce truc: #{iter.object_id}"
+            end
+        end
+        return nil
+    end
 end
 
