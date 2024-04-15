b54b0cad1097131fdc22a69691566da76d3f0a4d
1110lias <elias-b@outlook.fr>
2024-04-15 13:57:00 +0200

    First push empty project shell

---
+++ lib/user.rb
@@ -0,0 +1,20 @@
+require "pry"
+
+class User
+    attr_accessor :user_email, :user_age
+    @@all_user = []
+
+    def initialize (user_email_to_save, user_age_to_save)
+        @user_age = user_age_to_save
+        @user_email = user_email_to_save
+    end
+
+    def all
+        for User.each do iter
+            @@all_user << iter
+        end
+        puts @@all_user []
+    end
+    
+binding.pry
+end
